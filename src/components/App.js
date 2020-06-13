30038551ed5f375c317b0272c2aa7cc0be2e05a4
Erik Butcher <butcer0@gmail.com>
2020-06-12 20:19:08 -0400

    Introduced Song Details Smart Component
    Introduced Song Details Smart Component

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import SongList from "./SongList";
+import SongDetail from "./SongDetail";
 
 const App = () => {
     return (
         <div className="ui container grid">
             <div className="ui row">
-                <div className="column eight wide">
+                <div className="column seven wide">
                     <SongList />
+                </div>
+                <div className="column nine wide">
+                    <SongDetail />
                 </div>
             </div>
         </div>
